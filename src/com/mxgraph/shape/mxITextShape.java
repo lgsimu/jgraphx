97064612f950f1f073046f1888bb273ffc3d81a7
leiqiao <leiqiao0@aliyun.com>
2018-04-18 11:52:40 +0800

    init

---
+++ src/com/mxgraph/shape/mxITextShape.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2010, Gaudenz Alder, David Benson
+ */
+package com.mxgraph.shape;
+
+import com.mxgraph.canvas.mxGraphics2DCanvas;
+import com.mxgraph.view.mxCellState;
+
+import java.util.Map;
+
+public interface mxITextShape
+{
+  /**
+   *
+   */
+  void paintShape(mxGraphics2DCanvas canvas, String text, mxCellState state,
+                  Map<String, Object> style);
+}
